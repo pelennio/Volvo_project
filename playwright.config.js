b30f04bdbd23d8c3a966a046827bbd4d9df2b519
Olena <pelennio@gmail.com>
2025-11-17 19:43:24 -0600

    adding tesmo cli

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,9 @@
 const { defineConfig, devices } = require("@playwright/test");
+import { defineConfig } from "@playwright/test";
 
 module.exports = defineConfig({
   testDir: "./test",
+  reporter: [["list"], ["junit", { outputFile: "test-results/results.xml" }]],
   timeout: 30 * 1000,
   use: {
     // Set the base URL for all `page.goto()` calls when a path is used
